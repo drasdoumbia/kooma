ff8fd9b5fa6e44000f696c724587e6574f180eb1
drasdoumbia <subscript.d@gmail.com>
2020-12-27 15:28:24 +0000

    features: created the splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:your_splash/your_splash.dart';
 
 void main() {
   runApp(Kooma());
@@ -13,8 +14,47 @@ class Kooma extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(
-        child: Text("Hello in kooma"),
+      home: SplashScreen.timed(
+        seconds: 4,
+        route: MaterialPageRoute(builder: (_) => Home()),
+        body: Scaffold(
+          body: Container(
+            color: Color(0xFFFAD271),
+            child: Center(
+              child: Container(
+                width: 200.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    alignment: Alignment.center,
+                    image: AssetImage("assets/imgs/splash_screen_content.png"),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+            child: Text(
+          "Hello in kooma",
+          style: TextStyle(
+            fontSize: 18.0,
+            fontFamily: "Roboto",
+          ),
+        )),
       ),
     );
   }
